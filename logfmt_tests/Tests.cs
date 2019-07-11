9861ae9f29fa912b96c5b257d38e82002fa47080
Ken Haines <ken@kenhaines.net>
2019-07-11 12:21:52 -0700

    Adding unit test for KV pairs in log line

--- logfmt_tests/Tests.cs
+++ logfmt_tests/Tests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using logfmt;
 using Xunit;
@@ -23,6 +24,26 @@ namespace logfmt_tests
 
             Assert.Contains("msg=\"hello logs!\"", output);
 
+        }
+
+        [Fact]
+        public void LogOutputWithKVPairs()
+        {
+            var outputStream = new MemoryStream();
+            var logger = new Logger(outputStream);
+
+            // write a log entry
+            logger.Info("hello logs!",
+                new KeyValuePair<string, string>("color", "blue"),new KeyValuePair<string,string>("country","United States"));
+
+            outputStream.Seek(0,SeekOrigin.Begin);
+            var reader = new StreamReader(outputStream);
+
+            var output = reader.ReadLine();
+
+            Assert.Contains("msg=\"hello logs!\"", output);
+            Assert.Contains("color=blue", output);
+            Assert.Contains("country=\"United States\"", output);
         }
     }
 }
