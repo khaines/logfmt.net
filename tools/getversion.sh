04baf37d058327c8ea6db10bb19b0d212e048337
Ken Haines <ken@kenhaines.net>
2019-07-11 15:33:57 -0700

    adding version text script

---
+++ tools/getversion.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+set -o nounset
+set -o pipefail
+
+RELEASE_TAG=$(git describe --tags --exact-match 2>/dev/null)
+set -o errexit
+if [ -z "$RELEASE_TAG" ]
+then
+    WORKING_SUFFIX=$(if git status --porcelain | grep -qE '^(?:[^?][^ ]|[^ ][^?])\s'; then echo "-WIP"; else echo ""; fi)
+    BRANCH_PREFIX=$(git describe --dirty --always --tags)
+    echo "$BRANCH_PREFIX$WORKING_SUFFIX"
+else
+    echo "$RELEASE_TAG"
+fi
