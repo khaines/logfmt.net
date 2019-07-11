21796ba7468ee020d35ba8f07573d6b85e6920f2
Ken Haines <ken@kenhaines.net>
2019-07-11 12:26:36 -0700

    Adding unit test for invalid key name handling

--- logfmt_tests/Tests.cs
+++ logfmt_tests/Tests.cs
@@ -45,5 +45,28 @@ namespace logfmt_tests
             Assert.Contains("color=blue", output);
             Assert.Contains("country=\"United States\"", output);
         }
+
+        [Fact]
+        public void ExpectWarningWithInvalidKeyName()
+        {
+            var outputStream = new MemoryStream();
+            var logger = new Logger(outputStream);
+
+            // write a log entry, but use a KVPair key containing a space. There should be a warning entry added to the output stream
+            logger.Info("hello logs!",
+                new KeyValuePair<string, string>("not valid key", "blue"));
+
+            outputStream.Seek(0,SeekOrigin.Begin);
+            var reader = new StreamReader(outputStream);
+
+            var output = reader.ReadLine();
+            // this should be the warning
+            Assert.Contains(
+                "level=warn msg=\"Error in processing log request. Key field cannot contain spaces and has been truncated.\" invalid_key=\"not valid key\"", output);
+            output = reader.ReadLine();
+            // line we input, but with the trucated key name
+            Assert.Contains("level=info msg=\"hello logs!\" not=blue",output);
+
+        }
     }
 }
